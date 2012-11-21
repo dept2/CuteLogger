3d05cec0d12764ca62c4eb1ddc3fbab9d1ef8485
Nikolai Matyunin <matyunin.n@gmail.com>
2012-11-21 13:24:10 +0400

    Replace stripFunctionName function by the qCleanupFuncinfo function, backported from Qt5.

--- include/AbstractStringAppender.h
+++ include/AbstractStringAppender.h
@@ -37,6 +37,8 @@ class CUTELOGGERSHARED_EXPORT AbstractStringAppender : public AbstractAppender
                             const char* function, const QString& message) const;
 
   private:
+    static QByteArray qCleanupFuncinfo(const char*);
+
     QString m_format;
     mutable QReadWriteLock m_formatLock;
 };
