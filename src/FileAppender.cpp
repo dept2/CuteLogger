5139fcb8c0b45c9b9631cd9974b63203288b95df
unknown <stpa2497@ER00889N.ebgroup.elektrobit.com>
2013-10-14 16:46:33 +0200

    Implemented simple log file rotation.

--- src/FileAppender.cpp
+++ src/FileAppender.cpp
@@ -14,6 +14,9 @@
 // Local
 #include "FileAppender.h"
 
+// Qt
+#include <QDir>
+
 // STL
 #include <iostream>
 
@@ -30,6 +33,50 @@ FileAppender::FileAppender(const QString& fileName)
   setFileName(fileName);
 }
 
+//! Constructs a new file appender, which takes care of log file rotation.
+/**
+ * Creates a log file with the prefix <tt>logFilePrefix</tt>, the current timestamp and the filename extension '.log',
+ * for example: prefix_2013-10-10_11.55.17.log
+ *
+ * If the directory <tt>directory</tt> does not exist it will be created.
+ * If there are more than <tt>maxNrOfLogFiles</tt> log files in the directory <tt>directory</tt>,
+ * the oldest log files are deleted.
+ *
+ * \param logFilePrefix - a common prefix for all log files
+ * \param directory - the directory where to save the log files
+ * \param maxNrOfLogFiles - the maximum number of log files to keep
+ */
+FileAppender::FileAppender(const QString& logFilePrefix, const QString& directory, quint32 maxNrOfLogFiles)
+{
+  if(maxNrOfLogFiles < 1)
+  {
+    std::cerr << "<FileAppender::FileAppender> 'maxNrOfLogFiles' has to be greater than '1'" << std::endl;
+    return;
+  }
+
+  QDir dir(directory);
+
+  if(!dir.exists() && !dir.mkpath(directory))
+  {
+    std::cerr << "<FileAppender::FileAppender> Cannot create the log file directory " << qPrintable(directory) << std::endl;
+    return;
+  }
+
+  // get current log file count
+  dir.setSorting(QDir::Name | QDir::Reversed);
+  dir.setFilter(QDir::Files);
+  dir.setNameFilters(QStringList() << logFilePrefix + "*.log");
+  QFileInfoList logFiles = dir.entryInfoList();
+
+  // keep only the latest 'maxNrOfLogFiles - 1' files and delete all others
+  for(qint32 i = maxNrOfLogFiles - 1; i < logFiles.count(); ++i)
+  {
+    bool ret = QFile::remove(logFiles.at(i).absoluteFilePath());
+    Q_ASSERT(ret);
+  }
+
+  setFileName(directory + QDir::separator() + logFilePrefix + QDateTime::currentDateTime().toString("yyyy-MM-dd_hh.mm.ss") + ".log");
+}
 
 FileAppender::~FileAppender()
 {
