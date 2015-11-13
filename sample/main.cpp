1de3044df18c4f7e96b30d55f60793e73a080ad7
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-11-13 14:32:58 +0000

    working only with Qt5 now, also added example application

---
+++ sample/main.cpp
@@ -0,0 +1,21 @@
+#include <QCoreApplication>
+
+#include <Logger.h>
+#include <ConsoleAppender.h>
+
+int main(int argc, char* argv[])
+{
+  QCoreApplication app(argc, argv);
+  // ...
+  ConsoleAppender* consoleAppender = new ConsoleAppender();
+  consoleAppender->setFormat("[%{type}] <%{function}> %{message}\n");
+  logger->registerAppender(consoleAppender);
+  // ...
+  LOG_INFO("Starting the application");
+  int result = app.exec();
+  // ...
+  if (result)
+    LOG_WARNING() << "Something went wrong." << "Result code is" << result;
+
+  return result;
+}
