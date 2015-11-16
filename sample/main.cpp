41cd17ba58c726d70dbefd1a3ffad06d123e8d0f
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-11-16 11:45:09 +0000

    added CMake find file

--- sample/main.cpp
+++ sample/main.cpp
@@ -5,17 +5,18 @@
 
 int main(int argc, char* argv[])
 {
-  QCoreApplication app(argc, argv);
-  // ...
-  ConsoleAppender* consoleAppender = new ConsoleAppender();
-  consoleAppender->setFormat("[%{type}] <%{function}> %{message}\n");
-  logger->registerAppender(consoleAppender);
-  // ...
-  LOG_INFO("Starting the application");
-  int result = app.exec();
-  // ...
-  if (result)
-    LOG_WARNING() << "Something went wrong." << "Result code is" << result;
+//    QCoreApplication app(argc, argv);
+    // ...
+    ConsoleAppender* consoleAppender = new ConsoleAppender();
+    consoleAppender->setFormat("[%{type}] <%{function}> %{message}\n");
+    logger->registerAppender(consoleAppender);
+    // ...
+    LOG_INFO("Starting the application");
+//    int result = app.exec();
+    // ...
+//    if (result)
+    LOG_WARNING() << "Something went wrong.";
 
-  return result;
+//    return result;
+    return EXIT_SUCCESS;
 }
